371eaf546c5c7cc40940f9836dd42bafb0b5ed23
Jeffrey Kerley <jeffreykerley@jeffreys-mbp.attlocal.net>
2021-03-30 21:19:48 -0500

    fixed minor formatting, added movie data structure, set up backend for easy api calls, added slider buttons, and got active movie scrolling

--- App/StreamLet/src/app/home/home.page.ts
+++ App/StreamLet/src/app/home/home.page.ts
@@ -1,34 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Router,NavigationEnd} from '@angular/router';
 import { identifierModuleUrl } from '@angular/compiler';
 import { Platform } from '@ionic/angular';
+import { MovieServiceService } from '../services/movie-service.service';
+import { IonSlides} from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonSlides) slides: IonSlides;
+  
+  
+  
+  
   cards = [
     "Example","After",2,3,4,5,6,7,8,9,10,11,12,13,14,15
   ]
-  cardNumber = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2wT5ayfO9mk-9pghpeBf3_PevRid76Nk-Cg&usqp=CAU",
-    "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1574144362",
-    "https://i.pinimg.com/736x/6d/a3/07/6da307ddd2d2f5c2aaf1f09b112b3257.jpg",
-    "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/black_widow_ver9_xlg_240x360_crop_center.progressive.jpg?v=1598017338",
-    "https://www.digitalartsonline.co.uk/cmsdata/slideshow/3662115/star-wars-last-jedi-poster.jpg",
-    "https://i.redd.it/mkt3rwpuisz21.jpg",
-    "https://maxcdn.icons8.com/app/uploads/2019/05/film-poster-graphic-design.jpg",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "https://www.godisinthetvzine.co.uk/wp-content/uploads/2020/06/IMG_20200602_120716_501.jpg"
-  ]
+
+  //total movie types to presented on the main page
+  //first load fills these with the movie images
+  //this does not include the search function movies 
+  totalMovieGenres = {
+  reccomended : [],
+  watching : [],
+  releases : [],
+  comedies : [],
+  horror : [],
+  drama : [],
+  animated : [],
+  thriller : []
+  };
+  
+  //color array for underline categories 
   colors = [
     "darkred",
     "green",
@@ -37,11 +44,20 @@ export class HomePage {
     "darkyellow",
     "purple",
     "red",
-    "mediumblue"
+    "mediumblue",
+    "white",
+    "yellow",
+    "red",
+    "blue",
+    "darkgreen",
+    "cyan",
+    "darkcyan"
   ]
   width;
   height;
-  constructor(navCtrl: NavController, private router: Router, platform: Platform) {
+  constructor(navCtrl: NavController, private router: Router, 
+    platform: Platform, public movies: MovieServiceService,
+    ) {
     platform.ready().then(() => {
       this.width = platform.width();
       
@@ -50,13 +66,16 @@ export class HomePage {
   }
   ngAfterViewInit(){
     let posters = document.getElementsByClassName("movieImage");
-    let x= 0;
-    while(x<7){
-      posters[x].attributes[1].value = this.cardNumber[x]; 
-      x++
-    }
+    let x = 0;
+    
     
     this.setGenreColor();
+    this.totalMovieGenres = this.movies.getMovieImage(this.totalMovieGenres);
+    //set movie images of selected cards
+    while(x<15){
+      posters[x].attributes[1].value = this.totalMovieGenres.reccomended[x]; 
+      x++
+    }
   }
   routePage(name){
     this.router.navigate([name]);
@@ -84,8 +103,10 @@ export class HomePage {
       movieImage.style.backgroundPosition = "center";
     }
     cMain.style.right = "none";
+    const tempHeight = this.height/1.1;    
+    cMain.style.height = tempHeight.toString() + "px";
     
-    cMain.style.height = "85%";
+    
     this.populateCard(movieImage,currentWidth);
     
   }
@@ -99,11 +120,6 @@ export class HomePage {
     movieImage.style.height = widther.toString() + "px";
     
     
-  }
-  getMovieImage(cNum){
-    console.log(this.cardNumber[cNum]);
-    
-      return this.cardNumber[cNum];
   }
   resetNorm(){
     let animation = Math.floor(Math.random() * Math.floor(3));
--- App/StreamLet/src/app/services/movie-service.service.ts
+++ App/StreamLet/src/app/services/movie-service.service.ts
@@ -6,4 +6,33 @@ import { Injectable } from '@angular/core';
 export class MovieServiceService {
 
   constructor() { }
+  cardImage = [];
+  
+  getMovieImage(totalMovies){
+    //this is where api backend call happens to get movie images when first populating the page
+    //right now using psuedo data for testing 
+    this.cardImage = [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2wT5ayfO9mk-9pghpeBf3_PevRid76Nk-Cg&usqp=CAU",
+      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1574144362",
+      "https://i.pinimg.com/736x/6d/a3/07/6da307ddd2d2f5c2aaf1f09b112b3257.jpg",
+      "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/black_widow_ver9_xlg_240x360_crop_center.progressive.jpg?v=1598017338",
+      "https://www.digitalartsonline.co.uk/cmsdata/slideshow/3662115/star-wars-last-jedi-poster.jpg",
+      "https://i.redd.it/mkt3rwpuisz21.jpg",
+      "https://maxcdn.icons8.com/app/uploads/2019/05/film-poster-graphic-design.jpg",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "https://www.godisinthetvzine.co.uk/wp-content/uploads/2020/06/IMG_20200602_120716_501.jpg",
+    ]
+    //psuedo data is set in the movie object
+    totalMovies.reccomended = this.cardImage;
+
+    //returns total movie object 
+    return totalMovies;
+  }
+  
 }
